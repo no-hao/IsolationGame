637c6b079a8531b20c2520a89e790d39f7f42321
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-10-19 17:16:31 -0400

    Working on gui tweaks.

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, Canvas
 from .isolation import Isolation
 from .player import Player, HumanPlayer
 
@@ -8,40 +8,93 @@ class IsolationGUI:
         self.master = master
         self.master.title("Isolation Game")
 
+        # Player 1 Selection Frame
+        self.player1_selection_frame = tk.Frame(master)
+        self.player1_selection_frame.grid(row=0, column=0, columnspan=6, pady=(20, 10))
+
+        # Player 1 light indicator
+        self.player1_light = Canvas(self.player1_selection_frame, width=20, height=20, bg="white", relief="ridge")
+        self.player1_light.pack(side=tk.LEFT, padx=5)
+
+        # Player 1 selection in the frame
+        self.player1_label = tk.Label(self.player1_selection_frame, text="Player 1:")
+        self.player1_label.pack(side=tk.LEFT, padx=10)
+        self.player1_var = tk.StringVar(master)
+        self.player1_var.set("Human")  # default value
+        self.player1_dropdown = ttk.Combobox(self.player1_selection_frame, textvariable=self.player1_var, values=["Human", "Computer"])
+        self.player1_dropdown.pack(side=tk.LEFT, padx=10)
+
         # Create the game board
         self.cells = []
         for i in range(8):
             row = []
             for j in range(6):
                 cell = tk.Label(master, width=10, height=4, bg="white", relief="ridge")
-                cell.grid(row=i, column=j)
+                cell.grid(row=i+1, column=j)  # Offset by 1 to account for Player 1 dropdown
                 cell.bind("<Button-1>", self.handle_cell_click)
                 row.append(cell)
             self.cells.append(row)
 
-        # Frame for player selection dropdowns
-        self.selection_frame = tk.Frame(master)
-        self.selection_frame.grid(row=8, column=0, columnspan=6)
+        # Player 2 Selection Frame
+        self.player2_selection_frame = tk.Frame(master)
+        self.player2_selection_frame.grid(row=9, column=0, columnspan=6, pady=(10, 20))
 
-        # Player 1 selection in the frame
-        self.player1_label = tk.Label(self.selection_frame, text="Player 1:")
-        self.player1_label.pack(side=tk.LEFT, padx=10)
-        self.player1_var = tk.StringVar(master)
-        self.player1_var.set("Human")  # default value
-        self.player1_dropdown = ttk.Combobox(self.selection_frame, textvariable=self.player1_var, values=["Human", "Computer"])
-        self.player1_dropdown.pack(side=tk.LEFT, padx=10)
+        # Player 2 light indicator
+        self.player2_light = Canvas(self.player2_selection_frame, width=20, height=20, bg="white", relief="ridge")
+        self.player2_light.pack(side=tk.LEFT, padx=5)
 
         # Player 2 selection in the frame
-        self.player2_label = tk.Label(self.selection_frame, text="Player 2:")
+        self.player2_label = tk.Label(self.player2_selection_frame, text="Player 2:")
         self.player2_label.pack(side=tk.LEFT, padx=10)
         self.player2_var = tk.StringVar(master)
         self.player2_var.set("Human")  # default value
-        self.player2_dropdown = ttk.Combobox(self.selection_frame, textvariable=self.player2_var, values=["Human", "Computer"])
+        self.player2_dropdown = ttk.Combobox(self.player2_selection_frame, textvariable=self.player2_var, values=["Human", "Computer"])
         self.player2_dropdown.pack(side=tk.LEFT, padx=10)
 
-        # Start button below the frame
+        # Turn indicator and prompt in a separate frame
+        self.turn_frame = tk.Frame(master)
+        self.turn_frame.grid(row=10, column=0, columnspan=6)
+
+        self.turn_indicator = Canvas(self.turn_frame, width=20, height=20, bg="white", relief="ridge")
+        self.turn_indicator.pack(side=tk.LEFT, padx=5)
+        self.turn_prompt = tk.Label(self.turn_frame, text="Player 1's Turn to Move")
+        self.turn_prompt.pack(side=tk.LEFT, padx=10)
+
+        # Start and Restart buttons
         self.start_button = tk.Button(master, text="Start", command=self.start_game)
-        self.start_button.grid(row=9, column=0, columnspan=6)
+        self.start_button.grid(row=11, column=0, columnspan=3)
+        self.restart_button = tk.Button(master, text="Restart", command=self.restart_game, state="disabled")
+        self.restart_button.grid(row=11, column=3, columnspan=3)
+
+        # Legend and Action Log to the right of the board
+        self.side_frame = tk.Frame(master)
+        self.side_frame.grid(row=0, column=6, rowspan=12, sticky="ns")
+
+        legend_label = tk.Label(self.side_frame, text="Legend", font=("Arial", 12, "bold"))
+        legend_label.pack(pady=10)
+        legend_items = [("blue", "Player 1"), ("red", "Player 2"), ("#44463e", "Removed Token")]
+        for color, text in legend_items:
+            item_frame = tk.Frame(self.side_frame)
+            item_frame.pack(pady=5)
+            color_label = tk.Label(item_frame, width=5, height=2, bg=color, relief="ridge")
+            color_label.pack(side=tk.LEFT, padx=5)
+            text_label = tk.Label(item_frame, text=text)
+            text_label.pack(side=tk.LEFT)
+
+        # Action Log
+        self.action_log = tk.Listbox(self.side_frame, height=15)
+        self.action_log.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
+
+    # When updating the turn, we should also update the light indicators:
+    def update_turn_indicator(self):
+        # If player 1's turn
+        if self.game.current_player_index == 0:
+            self.player1_light.config(bg="green")
+            self.player2_light.config(bg="white")
+        # If player 2's turn
+        else:
+            self.player1_light.config(bg="white")
+            self.player2_light.config(bg="green")
 
     def refresh_board(self):
         for i in range(8):
@@ -50,7 +103,7 @@ class IsolationGUI:
                 if cell_value == 0:
                     self.cells[i][j].config(bg="white")  # Empty cell
                 elif cell_value == -1:
-                    self.cells[i][j].config(bg="grey")   # Removed token
+                    self.cells[i][j].config(bg="#44463e")   # Removed token
                 elif isinstance(cell_value, Player):
                     if cell_value == self.game.players[0]:
                         self.cells[i][j].config(bg="blue")   # Player 1
@@ -58,7 +111,7 @@ class IsolationGUI:
                         self.cells[i][j].config(bg="red")    # Player 2
 
     def handle_cell_click(self, event):
-        row, col = event.widget.grid_info()["row"], event.widget.grid_info()["column"]
+        row, col = event.widget.grid_info()["row"] - 1, event.widget.grid_info()["column"]  # Offset row by 1
         current_player = self.game.players[self.game.current_player_index]
 
         if not self.game.awaiting_token_removal:
@@ -107,6 +160,19 @@ class IsolationGUI:
         self.player1_dropdown.config(state="disabled")
         self.player2_dropdown.config(state="disabled")
         self.start_button.config(state="disabled")
+
+    def restart_game(self):
+        # Reset the board and game-related variables
+        self.game = None
+        for i in range(8):
+            for j in range(6):
+                self.cells[i][j].config(bg="white")
+        self.player1_dropdown.config(state="normal")
+        self.player2_dropdown.config(state="normal")
+        self.start_button.config(state="normal")
+        self.restart_button.config(state="disabled")
+        self.turn_prompt.config(text="Player 1's Turn to Move")
+        self.action_log.delete(0, tk.END)  # Clear the action log
 
     def display_game_over_message(self):
         # Display a game over message
